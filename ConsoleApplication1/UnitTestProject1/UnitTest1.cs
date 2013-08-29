21dd0276c94c2c1b6dad0019f55fa1339eb2e11c
danveith1 <dev6500@yahoo.com>
2013-08-29 17:47:01 -0400

    learning unit tests

---
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var json = ConsoleApplication1.Program.GetProfile("Jewpaltz");
+            Assert.IsNotNull(json);
+            Assert.IsTrue(json.Contains("Moshe"));
+            
+        }
+    }
+}
