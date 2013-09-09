a38c696ba754d1b68150d858b4729d1470ec32b7
danveith1 <dev6500@yahoo.com>
2013-09-09 17:45:02 -0400

    practicing testing
    and some functional stuff

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -1,10 +1,13 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 
 namespace ConsoleApplication1
 {
     public class Program
     {
+        public delegate int Multiply(int x, int y);
+        
         public static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World");
@@ -12,7 +15,7 @@ namespace ConsoleApplication1
             System.Console.WriteLine("What is your username?");
             string response = System.Console.ReadLine();
 
-            
+
 
             System.Console.WriteLine("Here's your profile info");
             System.Console.Write(GetProfile(response));
@@ -26,4 +29,40 @@ namespace ConsoleApplication1
             return json;
         }
     }
+        public class Dog
+        {
+            private List<string> _ListOfWords = new List<string>();
+            private string _DogsWord = "woof";
+
+            public string DogsWord 
+            {
+                get
+                {
+                    return _DogsWord;
+                }
+                set
+                {
+                    _DogsWord = value;
+                }
+            }
+
+            public string Bark()
+            {
+                return _DogsWord + " " + string.Join(", ", _ListOfWords);
+            }
+            public void LearnNewWord(string word)
+            {
+                _ListOfWords.Add(word);
+
+  
+            }
+            [Obsolete]
+            public string Run()
+            {
+                return "I am running at " + Speed + " Miles an Hour";
+            }
+
+            public string Speed { get; set; }
+        }
+    
 }
--- ConsoleApplication1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
 
 namespace UnitTestProject1
 {
@@ -14,5 +15,74 @@ namespace UnitTestProject1
             Assert.IsTrue(json.Contains("Moshe"));
             
         }
+        [TestMethod]
+        public void TestThatDogBarks()
+        {
+            var d = new Dog();
+            var actual = d.Bark();
+            Assert.AreEqual("woof", d.Bark());
+
+        }
+        [TestMethod]
+        public void TestThatDogCanSayOtherThings()
+        {
+            var d = new Dog();
+         
+            d.DogsWord = "meyow";
+
+            d.LearnNewWord("Hello");
+            d.LearnNewWord("How Are You");
+            
+            var actual = d.Bark();
+            Assert.AreEqual("meyow", actual);
+
+        }
+        [TestMethod]
+        public void TestThatDogCanSayLotsOfThings()
+        {
+            var d = new Dog();
+
+            d.DogsWord = "meyow";
+            var actual = d.Bark();
+            Assert.AreEqual("meyow", actual);
+
+        }
+        [TestMethod]
+        public void TestThatDogCanRunAtAnySpeed()
+        {
+            var d = new Dog();
+
+            d.Speed = "200";
+            var actual = d.Run();
+            Assert.AreEqual("I am running at 200 Miles an Hour", actual);
+
+        }
+        [TestMethod]
+        public void TestDelegate()
+        {
+            
+            // not sure why I have to use the ConsoleApplication.Program before Multiply.
+            ConsoleApplication1.Program.Multiply MyFunc = delegate(int x, int y)
+            {
+                return x * y;
+            };
+
+            Assert.AreEqual(4, MyFunc(2, 2));
+
+            MyFunc = (x, y) => x * y;
+            // look into this syntax
+        
+        }
+        [TestMethod]
+        public void TestDelegate()
+        {
+            Func<int, int> Squared = x => x * x;
+
+            Assert.AreEqual(4, Squared(2));
+            Assert.AreEqual(9, Squared(3));
+
+        }
+
     }
+
 }
