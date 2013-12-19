42424352bc28f43773b5a2b53198121784e532e9
danveith1 <dev6500@yahoo.com>
2013-12-19 12:08:07 -0500

    some more changes
    cities function grabs json turns it into string array

--- practice/practice/Program.cs
+++ practice/practice/Program.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
 using System.Net.Http;
-namespace FinalExam
+using System.Runtime.Serialization.Json;
+using Newtonsoft.Json;
+
+
+namespace practice
 {
-    class Cities
+    class Program
     {
-        static async Task<string[]> MainAsync(String text)
+        static void Main()
+        {
+            MainAsync().Wait();
+        }
+        static async Task<string[]> MainAsync()
         {
 
 
@@ -17,17 +24,17 @@ namespace FinalExam
             String[] output = new String[50];
 
             Console.WriteLine("letter");
-            String input = "http://cs.newpaltz.edu/~plotkinm/2012Grad/Final/api/Cities.php?term=" + text;
+            String input = "http://cs.newpaltz.edu/~plotkinm/2012Grad/Final/api/Cities.php?term=" + Console.ReadLine();
             try
             {
                 HttpResponseMessage response = await client.GetAsync(input);
                 response.EnsureSuccessStatusCode();
-
+                
                 var responseBody = await response.Content.ReadAsStringAsync();
 
                 output = JsonConvert.DeserializeObject<string[]>(responseBody);
-
-
+                
+                
             }
             catch (HttpRequestException e)
             {
@@ -36,8 +43,9 @@ namespace FinalExam
             }
             return output;
             //client.Dispose(true);
-
-
+            
+            
         }
+        
     }
 }
